d87a8628a42899ca21684c179697a8f71f604141
satishjampalwar <Lg-optimus1>
2018-03-29 11:22:37 +0530

    Result Text size changed

--- src/RowMatch.js
+++ src/RowMatch.js
@@ -98,10 +98,10 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
   },
   cardResultText :{
-    fontWeight:'400',
+    fontWeight:'600',
     padding:2,
     color:'red',
-    fontSize:15,
+    fontSize:17,
     alignSelf: 'center',
   },
   imageMatchCard: {
